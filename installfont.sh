555754718be7d190c5d7193e5d8b6e4feb8327d0
simon zheng <simon.zheng@mujin.co.jp>
2018-06-27 16:17:47 +0900

    install unzip

--- installfont.sh
+++ installfont.sh
@@ -4,7 +4,10 @@ I1FS=$'\n\t'
 mkdir -p /tmp/adodefont
 cd /tmp/adodefont
 wget -q --show-progress -O source-code-pro.zip https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
+
+sudo apt-get update && sudo apt-get install unzip
 unzip -q source-code-pro.zip -d source-code-pro
+
 mkdir -p ~/.fonts
 cp -v source-code-pro/*/OTF/*.otf ~/.fonts/
 fc-cache -f
